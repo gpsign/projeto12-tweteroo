a4032d51481d9a38b6c3e3f74fb173c1b5856c0a
gpsign <gpsignorini@gmail.com>
2023-06-25 23:40:17 -0300

    gets

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let tweets = [];
 let usuarios = [];
@@ -17,7 +18,22 @@ app.post('/tweets', (req, res) => {
     if (usuario == undefined) {
         res.send(401);
     }
-    else { tweets.push({ username: req.body.username, avatar: usuario.avatar, tweet: req.body.tweet }); res.send(200) }
+    else {
+        tweets.push({ username: req.body.username, avatar: usuario.avatar, tweet: req.body.tweet });
+        res.send(200)
+    }
+});
+
+app.get("/tweets", (req, res) => {
+    let nmrTweets = tweets.length > 10 ? 10 : tweets.length;
+    if (nmrTweets == 0) res.send([]);
+
+    let ultimosTweets = [];
+
+    for (let i = 0; i < nmrTweets; i++) {
+        ultimosTweets.push(tweets[i]);
+    }
+    res.send(ultimosTweets);
 });
 
 app.listen(5000);
